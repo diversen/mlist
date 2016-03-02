6743b9b6d55703962d210c9d15f83f1537342c8b
Dennis Iversen <dennis.iversen@gmail.com>
2016-03-02 12:04:33 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,7 +2,6 @@
 
 $LANG = array();
 
-
 // Translation of file modules/mlist/install.inc
 
 $LANG['Mailing list'] = 'Mailing list';
@@ -18,6 +17,8 @@ $LANG['Mailing lists'] = 'Mailing lists';
 $LANG['Send mail'] = 'Send mail';
 $LANG['To'] = 'To';
 $LANG['List has been added to queue'] = 'List has been added to queue';
+$LANG['Send mail to list'] = 'Send mail to list';
+$LANG['Mail is already added to the queue'] = 'Mail is already added to the queue';
 $LANG['Send to list'] = 'Send to list';
 $LANG['Send mail to mailing-list'] = 'Send mail to mailing-list';
 $LANG['Edit email'] = 'Edit email';
@@ -26,11 +27,12 @@ $LANG['Generate exports'] = 'Generate exports';
 $LANG['Write in markdown'] = 'Write in markdown';
 $LANG['Save'] = 'Save';
 $LANG['Mail was created'] = 'Mail was created';
-$LANG['Send single email'] = 'Send single email';
 $LANG['View'] = 'View';
 $LANG['Edit'] = 'Edit';
 $LANG['Delete'] = 'Delete';
+$LANG['Send single email'] = 'Send single email';
 $LANG['Delete mail'] = 'Delete mail';
+$LANG['System could not send email'] = 'System could not send email';
 $LANG['Create list'] = 'Create list';
 $LANG['Create new list'] = 'Create new list';
 $LANG['List was created'] = 'List was created';
